ae76a0e0d0467884b9c6c2c8df6d344452ee7854
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2023-03-18 17:11:36 -0500

    centralBar

--- src/Frontend/Main.jsx
+++ src/Frontend/Main.jsx
@@ -1,35 +1,20 @@
 import React from 'react';
 import Box from '@mui/material/Box';
-import Grid from '@mui/material/Grid';
-import Target from './MainHelpers/Target';
+import Target from './MainHelpers/Targets/Target';
+import CentralBar from './MainHelpers/CentralBar';
 
 const Main = () => {
   return (
-    <Box sx={{ display:'flex',flexDiection:'column', height: 'fit-content', width: '100%' }}>
-
-      <Grid container direction='column'>
-        <Grid container direction="row">
-           <Grid item xs={12} sx={{display:'flex',justifyContent:'space-between'}} >
-             <Target />
-             <Target />
-             <Target />
-             <Target />
-             <Target />
-           </Grid>
-        </Grid>
-
-
-        <Grid container direction="row">
-           <Grid item xs={12} sx={{display:'flex',justifyContent:'space-between'}} >
-             <Target />
-             <Target />
-             <Target />
-             <Target />
-             <Target />
-          </Grid>
-        </Grid>
-
-      </Grid>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        height: 'fit-content',
+        width: '100%',
+      }}
+    >
+      <Target />
+      <CentralBar />
     </Box>
   );
 };
---
+++ src/Frontend/MainHelpers/CentralBar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+
+const CentralBar = () => {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        width: '100%',
+        height: '100px',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Box
+        sx={{
+          display: 'flex',
+          bgcolor: 'steelblue',
+          width: '80%',
+          height: '70%',
+          justifyContent: 'center',
+          alignItems: 'center',
+          borderRadius: '10px',
+        }}
+      >
+        <Button sx={{ color: 'black' }}>Leído</Button>
+        <Button sx={{ color: 'black' }}>Siguiendo</Button>
+        <Button sx={{ color: 'black' }}>Favoritos</Button>
+        <Button sx={{ color: 'black' }}>Abandonados</Button>
+      </Box>
+    </Box>
+  );
+};
+
+export default CentralBar;
