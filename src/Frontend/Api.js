0c475173d901f36a0b8537c441e0c2a637504494
velasquezgalejandro <alejandro.velasquez2004@gmail.com>
2023-03-31 16:03:30 -0500

    appi

--- src/Frontend/Api.js
+++ src/Frontend/Api.js
@@ -37,3 +37,24 @@
 //   .catch(function (error) {
 //     console.error(error);
 //   });
+
+const axios = require('axios');
+
+const options = {
+  method: 'GET',
+  url: 'https://community-manga-eden.p.rapidapi.com/manga/%7Bmanga.id%7D',
+  params: { 'manga.id': '4e70e9f6c092255ef7004336' },
+  headers: {
+    'X-RapidAPI-Key': 'SIGN-UP-FOR-KEY',
+    'X-RapidAPI-Host': 'community-manga-eden.p.rapidapi.com',
+  },
+};
+
+axios
+  .request(options)
+  .then(function (response) {
+    console.log(response.data);
+  })
+  .catch(function (error) {
+    console.error(error);
+  });
